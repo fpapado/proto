a4d6f08e1d0938649f87edfa6f58897699bce449
fpapado <fotis@futurice.com>
2018-06-03 21:56:21 +0300

    Style link, why not

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,32 @@ import React from 'react';
 import Div from '../elements/Div';
 import H1 from '../elements/H1';
 import P from '../elements/P';
+import A from '../elements/A';
+import theme from '../theme';
 
 const Index = props => (
-  <Div bg="bg" py={[5, 6, 7]} px={[4, 5, 6]}>
+  <Div color="black.1" bg="bg" py={[5, 6, 7]} px={[4, 5, 6]}>
     <H1 fontSize={[2, 4, 6]} color="fuschia.7">
       Proto
     </H1>
-    <P fontSize={[2, 3]} color="blue.6" lineHeight="copy">
+    <P fontSize={[2, 3]} lineHeight="copy">
       Setup for prototyping static sites with x0, emotion, styled-system and
       react.
-    </P>
-    <P fontSize={[2, 3]} color="blue.6" lineHeight="copy">
+      <br />
       Read the docs for more information:
       <br />
-      <a href="https://github.com/fpapado/proto">
+      <A
+        color="blue.5"
+        hover={{
+          color: 'blue.7',
+          textDecorationColor: theme.colors.blue[7],
+        }}
+        active={{ color: 'blue.5' }}
+        css={{ textDecorationColor: theme.colors.blue[2] }}
+        href="https://github.com/fpapado/proto"
+      >
         https://github.com/fpapado/proto
-      </a>
+      </A>
     </P>
   </Div>
 );
